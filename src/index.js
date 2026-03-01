3afb366dce1a56f6abb3211f242f24492ac2aa74
Yakultttph <eleahcamillevcarillo@gmail.com>
2026-03-01 17:21:55 +0800

    Switch to HashRouter for deployment-safe routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { ShopProvider } from './context/ShopContext';
@@ -8,10 +8,10 @@ import { ShopProvider } from './context/ShopContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ShopProvider>
         <App />
       </ShopProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
